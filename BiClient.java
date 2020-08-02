b0cb7b4be7c3ab2e84c0938151292acbe1065f16
SHAMIL T <tshamil90@gmail.com>
2020-08-02 09:41:20 +0530

    Server Class File

---
+++ BiClient.java
@@ -0,0 +1,46 @@
+//Input OutPut Class for Bufferedreader&PrintWriter Classes
+import java.io.*;
+//Network Class for Socket
+import java.net.*;
+
+class BiClient 
+{
+    public static void main(String a[]) throws Exception
+    {
+        //For Connecting to Port
+        //Server:- Localhost
+        //Port number:-9000 
+        Socket s = new Socket("localhost",9000);
+        System.out.println("Server Connected");
+
+        //Read Message From KeyBoard
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //Read Message from Server
+        BufferedReader ir = new BufferedReader(new InputStreamReader(s.getInputStream()));        
+
+        //Sending Data to the Server
+        PrintWriter pr = new PrintWriter(s.getOutputStream(),true);
+
+        //while loop for sending and reading messages
+        while (true) {
+            System.out.println("Enter the message");
+            String str = br.readLine();//read the message
+            //send message
+            pr.println(str);
+            if(str.equals("bye"))
+            {
+                break;
+            }
+            //read message from Server
+            String msg = ir.readLine();
+            System.out.println("Server :-"+msg);
+            if(msg.equals("bye"))
+            {
+                break;
+            }
+        }
+        //Closing The Connection
+        System.out.println("Clossing Connection");
+        s.close();
+    }
+} 
---
+++ BiServer.java
@@ -0,0 +1,45 @@
+//Input OutPut Class for Bufferedreader&PrintWriter Classes
+import java.io.*;
+//Network Class for Socket&ServerSocket
+import java.net.*;
+
+class BiServer
+{
+    public static void main(String a[]) throws Exception
+    {
+        //Establishing Connection
+        ServerSocket ss = new ServerSocket(9000);
+        System.out.println("Waiting for Client");
+        Socket s = ss.accept();
+        System.out.println("Client Connected");
+
+        //For Reading Message from Client
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));          
+        //For Reading Message from Keyboard
+        BufferedReader ir = new BufferedReader(new InputStreamReader(System.in));
+        //Sending Data to the Client
+        PrintWriter pr = new PrintWriter(s.getOutputStream(),true);
+
+        //while loop for sending and reading messages
+        while (true) {
+            //read message from Client
+            String msg = br.readLine();
+            System.out.println("Client :-"+msg);
+            if(msg.equals("bye"))
+            {
+                break;
+            }
+            System.out.println("Enter the message");
+            String str = ir.readLine();//read the message
+            //send message
+            pr.println(str);
+            if(str.equals("bye"))
+            {
+                break;
+            }
+        }
+        //Closing The Connection
+        System.out.println("Clossing Connection");
+        s.close();
+    }
+}
